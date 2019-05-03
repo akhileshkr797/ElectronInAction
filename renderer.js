5e30e91a57f605aa7a73cbeae466e19f15a17b69
akhileshkr797 <akhileshkr797@gmail.com>
2019-05-03 09:59:57 +0530

    added open-file and rendering its content

--- renderer.js
+++ renderer.js
@@ -20,3 +20,43 @@ markdownView.addEventListener('keyup', (event) => {
 const currentContent = event.target.value;
 renderMarkdownToHtml(currentContent);
 });
+
+//open-file button
+openFileButton.addEventListener('click', () => {
+    alert('You clicked OPEN_FILE button')
+})
+
+/*
+//importing a function from basic-math.js
+const basicMath = require('./basic-math')
+const m = basicMath.addTwo(4)
+markdownView.innerHTML = m
+
+*/
+
+/*
+//requiring functions from main process
+const {remote} = require('electron')
+const mainProcss = remote.require('./main.js')
+
+//Trigger getFileFromUser() in main.js from UI
+openFileButton.addEventListener('click', ()=>{
+    mainProcss.getFileFromUser()
+})
+
+*/
+
+// receiving the contents from main Procss and displaying it in markdown-textarea
+const {remote, ipcRenderer} = require('electron')
+const mainProcss = remote.require('./main.js')
+
+//Trigger getFileFromUser() in main.js from UI
+openFileButton.addEventListener('click', ()=>{
+    mainProcss.getFileFromUser()
+})
+
+//listing for message on the file-opened channel
+ipcRenderer.on('file-opened', (event, file, content)=>{
+    markdownView.value = content
+    renderMarkdownToHtml(content)
+})
